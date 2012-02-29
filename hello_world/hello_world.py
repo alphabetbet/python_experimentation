62b2b918c56f3f8f7623f831a14addb6b2821528
Jean-Baptiste Guet <alphabetbet@hotmail.com>
2012-02-29 23:15:28 +0100

    first hello worlds.

---
+++ hello_world/hello_world.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+
+the_world = 42
+if the_world:
+        print "Yo man, peace and love man!"
---
+++ hello_world/hello_world_currency.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python
+# -*- coding: iso-8859-15 -*-
+
+currency = u"€"
+print ord(currency)
